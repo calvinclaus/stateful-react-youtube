79dcd89c931cfdf12dc5888da8f94072051de729
calvinclaus <calvinclaus@me.com>
2016-09-27 23:29:09 +0200

    build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,60 @@
 var DefinePlugin = require('webpack').DefinePlugin;
 var env = process.argv[2] !== "-p" ? '"development"' : '"production"'; 
-module.exports = {
-  entry: {
-    library: './src/entry.js',
-    examples: './examples/entry.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: './build',
-    library: "stateful-react-youtube",
-    libraryTarget: "umd",
-    umdNamedDefine: true,
-    devtool: 'source-map'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(noode_modules)/,
-        loader: 'babel-loader'
-      },
-      { test: /\.css$/,  loader: "style-loader!css-loader" }
+var build = process.argv[3] === "-b"; 
+if (build) {
+  module.exports = {
+    entry: './src/entry.js',
+    output: {
+      filename: 'library.js',
+      path: './build',
+      libraryTarget: "umd",
+      umdNamedDefine: true
+    },
+    externals: {
+      'react': {
+        'commonjs': 'react',
+        'commonjs2': 'react',
+        'amd': 'react',
+        'root': 'React'
+      }
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(noode_modules)/,
+          loader: 'babel-loader'
+        },
+        { test: /\.css$/,  loader: "style-loader!css-loader" }
+      ]
+    },
+    plugins: [
+      new DefinePlugin({
+        "process.env.NODE_ENV":env
+      }),
     ]
-  },
-  plugins: [
-    new DefinePlugin({
-      "process.env.NODE_ENV":env
-    })
-  ]
+  }
+} else {
+  module.exports = {
+    entry: './examples/entry.js',
+    output: {
+      filename: 'examples.js',
+      path: './build',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(noode_modules)/,
+          loader: 'babel-loader'
+        },
+        { test: /\.css$/,  loader: "style-loader!css-loader" }
+      ]
+    },
+    plugins: [
+      new DefinePlugin({
+        "process.env.NODE_ENV":env
+      }),
+    ]
+  }
 }
